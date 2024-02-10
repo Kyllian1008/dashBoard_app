1f53d5d05dbb142299ab1b22bb42a3a620ec5f2b
Kyllian ROUSSELEAU <kyllianr1008@gmail.com>
2024-02-10 17:01:54 +0100

    commit_10_02_2024

---
+++ main.js
@@ -0,0 +1,23 @@
+const menuOpen = document.getElementById('menu-open');
+const menuClose = document.getElementById('menu-close');
+const sideBar = document.querySelector('.container .left-section');
+const sidebarItems = docuement.querySelectorAll('.container .left-section .sidebar .item');
+
+menuOpen.addEventListener('click', ()=> {
+    sideBar.style.top = '0';
+});
+
+menuOpen.addEventListener('click', ()=> {
+    sideBar.style.top = '-60vh';
+});
+
+let activeItem = sidebarItems[0];
+
+sidebarItems.forEach(element =>{
+    element.addEventListener('click', ()=>{
+        if (activeItem){
+            activeItem.removeAttribute('id');
+        }
+        element.setAttribute('id', 'active')
+    })
+})
